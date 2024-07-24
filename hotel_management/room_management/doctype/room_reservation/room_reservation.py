fb18868e09e34be313169557b42c42ca15e7bde0
estifan <danwondimu00@gmail.com>
2024-07-24 21:14:06 +0300

    update

--- hotel_management/room_management/doctype/room_reservation/room_reservation.py
+++ hotel_management/room_management/doctype/room_reservation/room_reservation.py
@@ -19,8 +19,9 @@ class RoomReservation(Document):
 		print("before_save")
 		for reservation in self.rooms_for_reservation:
 			sReservation = frappe.get_all('Single Reservations', filters={'room': reservation.room ,"reservation_date": ["between",  (reservation.starts_on, reservation.ends_on)]}, fields=['child_table_id','reservation_date'])
+			print("len: ",len(sReservation))
+			print("sReservation: ", sReservation)
 			if(len(sReservation)):
-				# frappe.msgprint(f"Room {reservation.room} alradiy reserved on {sReservation[0].reservation_date}.","Error")
 				if(sReservation[0].child_table_id == reservation.name ):
 					pass
 				else:
@@ -32,30 +33,32 @@ class RoomReservation(Document):
 	def on_update(self):
 		print("on_update")
 		for reservation in self.rooms_for_reservation:
-			print("reservation",reservation)
-			changeRoomStatus(reservation.room,"Reserved")
-			start = datetime.strptime(reservation.starts_on, '%Y-%m-%d')
-			ends_on = datetime.strptime(reservation.ends_on, '%Y-%m-%d')
-			delta = timedelta(days=1)
-			while start <= ends_on:
-				try:
-					single_r = frappe.get_doc(
-						{
-							"doctype": "Single Reservations",
-							"type": reservation.type,
-							"room": reservation.room,
-							"price": reservation.price,
-							"reservation_date": start,
-							"room_reservation_id": reservation._parent_doc,
-							"child_table_id": reservation.name,
-						}
-					)
-					data = single_r.insert()
-					frappe.db.commit()
-					start += delta
-				except:
-					start += delta
-					pass
+			sReservation = frappe.get_all('Single Reservations', filters={'room': reservation.room ,"reservation_date": ["between",  (reservation.starts_on, reservation.ends_on)]}, fields=['child_table_id','reservation_date'])
+			if(len(sReservation) == 0):
+				print("reservation",reservation)
+				changeRoomStatus(reservation.room,"Reserved")
+				start = datetime.strptime(reservation.starts_on, '%Y-%m-%d')
+				ends_on = datetime.strptime(reservation.ends_on, '%Y-%m-%d')
+				delta = timedelta(days=1)
+				while start <= ends_on:
+					try:
+						single_r = frappe.get_doc(
+							{
+								"doctype": "Single Reservations",
+								"type": reservation.type,
+								"room": reservation.room,
+								"price": reservation.price,
+								"reservation_date": start,
+								"room_reservation_id": reservation._parent_doc,
+								"child_table_id": reservation.name,
+							}
+						)
+						data = single_r.insert()
+						frappe.db.commit()
+						start += delta
+					except:
+						start += delta
+						pass
 	def after_insert(self):
 		print("after_insert")
 		for reservation in self.rooms_for_reservation:
