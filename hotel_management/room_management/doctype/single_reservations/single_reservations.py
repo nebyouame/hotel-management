3b6bdc0c580f5e5acee2cc1f9e1bf6ce1f82310f
estifan <danwondimu00@gmail.com>
2024-07-25 13:45:54 +0300

    update

--- hotel_management/room_management/doctype/single_reservations/single_reservations.py
+++ hotel_management/room_management/doctype/single_reservations/single_reservations.py
@@ -2,8 +2,13 @@
 # For license information, please see license.txt
 
 # import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class SingleReservations(Document):
-	pass
+	def validate(self):
+		Reserved_Room = frappe.get_all('Reserved Room', filters={'name': self.child_table_id},fields=['parent'])
+		print("Reserved_Room: ",Reserved_Room[0])
+		self.room_reservation_id = Reserved_Room[0].parent
+
