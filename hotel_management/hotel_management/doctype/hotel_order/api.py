d198c48bc93a20cf908af86333c26ecfbd84a03d
Nebyou <nebyouame737@gmail.com>
2024-10-27 15:09:35 +0000

    stock integration added part 4

--- hotel_management/hotel_management/doctype/hotel_order/api.py
+++ hotel_management/hotel_management/doctype/hotel_order/api.py
@@ -1,42 +1,37 @@
-
 from __future__ import unicode_literals
 import frappe
 
-
 @frappe.whitelist()
 def update_stock_entry_qty(menu_name, qty):
-        qty_to_deduct = float(qty) 
-       
-        items_linked_to_menu = frappe.get_list('Item', 
-                                               filters={'menuu': menu_name},
-                                               fields=['name', 'item_code'])
+    qty_to_deduct = float(qty)
+    
+    
+    items_linked_to_menu = frappe.get_list('Item', 
+                                           filters={'menuu': menu_name},
+                                           fields=['name', 'item_code'])
 
-        if items_linked_to_menu:
-         
-            for linked_item in items_linked_to_menu:
-               
-                stock_entries = frappe.get_list('Stock Entry Detail', 
-                                                filters={'item_code': linked_item['item_code']},
-                                                fields=['parent', 'name', 'qty'])
+    if items_linked_to_menu:
+        for linked_item in items_linked_to_menu:
+            
+            stock_entries = frappe.get_list('Stock Entry Detail', 
+                                            filters={'item_code': linked_item['item_code']},
+                                            fields=['name', 'qty'])
 
-                if stock_entries:
-                    for entry in stock_entries:
+            if stock_entries:
+                for entry in stock_entries:
+                   
+                    stock_entry_detail = frappe.get_doc('Stock Entry Detail', entry['name'])
+
+                  
+                    if stock_entry_detail.item_code == linked_item['item_code']:
+                        new_qty = stock_entry_detail.qty - qty_to_deduct
+
                        
-                        stock_entry = frappe.get_doc('Stock Entry', entry['parent'])
-                        
-                        
-                        stock_entry_detail = stock_entry.getone({'item_code': linked_item['item_code']})
-                        
-                        
-                        if stock_entry_detail:
-                            new_qty = stock_entry_detail.qty - qty_to_deduct
-                            if new_qty < 0:
-                            
-                                frappe.throw(f"Cannot deduct {qty} from {stock_entry_detail.qty}. Insufficient quantity.")
-                            else:
-                                stock_entry_detail.qty = new_qty
-                                
-                                stock_entry.save(ignore_permissions=True)
+                        if new_qty < 0:
+                            frappe.throw(f"Cannot deduct {qty} from {stock_entry_detail.qty}. Insufficient quantity.")
+                        else:
+                            stock_entry_detail.qty = new_qty
+                            stock_entry_detail.save(ignore_permissions=True) 
 
-                    return True 
-        return False
+        return True
+    return False
